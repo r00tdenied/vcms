bf1e0003eea50669eb8b7a96ab46877ea65e414a
r00tdenied <josh@ariel.net>
2011-07-14 13:25:26 -0700

    page controller additions

--- index.php
+++ index.php
@@ -24,12 +24,20 @@ if (PageURL () == 'http://qa-vcms/') { include "home.php"; }
 // Page Control
 switch ($_GET[p])
 {
-	//Admin pages
+	//Admin Control pages
 	case 'acp': include "admin/main.php"; break;
 	case 'acp-users': include "admin/users.php"; break;
 	case 'acp-channels': include "admin/channels.php"; break;
 	case 'acp-categories': include "admin/categories.php"; break;
 	case 'acp-prefix': include "admin/prefix.php"; break;
+	
+	//User Control Pages
+	case 'ucp': include "users/main.php"; break;
+	case 'ucp-profile': include "users/profile.php"; break;
+	
+	//Main Pages
+	case 'itemview'; include "itemview.php"; break;
+	case 'portal'; include "portal.php"; break;
 }
 
 // Footer Page Elements
