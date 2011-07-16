2e37ca83eb4a52d3f61e9cccc2fc0d635b1660ab
r00tdenied <josh@ariel.net>
2011-07-16 14:41:10 -0700

    added limit to $numSkus

--- skugen.php
+++ skugen.php
@@ -18,8 +18,8 @@ while ($row = mysql_fetch_assoc($item_prefix)) {
 ?>
 </select>
 </td></tr>
-<tr><td># items to create:</td><td><input type="text" name="numSkus" /></td></tr>
-<tr><td>Build with item variation:</td><td><select name='varFlag'><option value='0'>Disabled</option><option value='1'>Enabled</option></select>
+<tr><td>Total items to create:</td><td><input type="text" name="numSkus" /></td></tr>
+<tr><td>Build items with variation:</td><td><select name='varFlag'><option value='0'>Disabled</option><option value='1'>Enabled</option></select>
 <tr><td colspan='2'><input type="submit"></td></tr>
 </form>
 
@@ -32,7 +32,10 @@ $varFlag = $_POST['varFlag'];
 if ($numSkus == 0){
 echo '<tr><td colspan="2"><font color="red">Please enter the number of skus to generate!</font></td></tr>';
 }
-elseif ($numSkus > 1){
+elseif ($numSkus >=1001){
+echo '<tr><td colspan="2"><font color="red">You are not able to generate that many skus!</font></td></tr>';
+}
+else {
 	echo '<tr><td colspan="2"><font color="green">You entered the correct number</font></td></tr>';
 	echo '<tr><td colspan="2">You entered '. $numSkus .' skus and selected the '. $catPref .' prefix</td></tr>';
 	$topPrefNum_check =  'Select parent_sku FROM item_alloc where sku_prefix ="'.$catPref.'" order by parent_sku desc limit 1';
@@ -41,12 +44,14 @@ elseif ($numSkus > 1){
 	$topPrefNum = explode('-',$topPrefNum['parent_sku']);
 	//echo $topPrefNum[1];
 	
-	while($i <= $numSkus) {
+	while($i < $numSkus) {
 	$topPrefNum[1] = $topPrefNum[1]+1;
 	$topPrefNum[1] = str_pad($topPrefNum[1],6,"0", STR_PAD_LEFT);
 
 	$newSku = implode('-', array($catPref, $topPrefNum[1]));
 	echo '<tr><td>Generated: </td><td>'.$newSku.'</td></tr>';
+	mysql_query("INSERT INTO item_master (item_sku,parent_sku,variant_flag) VALUES ('$newSku','$newSku','$varFlag')");
+	mysql_query("INSERT INTO item_alloc (parent_sku,sku_prefix,sku_number,status) VALUES ('$newSku','$catPref','$topPrefNum[1]','NEW')");
 	$i++;
 	}
 }
