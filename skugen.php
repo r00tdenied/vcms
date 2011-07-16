d916e93ffed6b502095520afe9be0c0c4b95d6e3
r00tdenied <josh@ariel.net>
2011-07-16 12:59:02 -0700

    sku gen logic

--- skugen.php
+++ skugen.php
@@ -6,8 +6,6 @@ dbconn(DB_ADDRESS, DB_NAME, DB_USER, DB_PASSWORD);
 $item_prefix_query='SELECT * from item_prefix';
 $item_prefix=mysql_query($item_prefix_query,$DbLink);
 
-$topSkuNum = '000001';
-
 ?>
 <form action="" method="post">
 <table style='margin-left: auto; margin-right: auto; width: 600px;' >
@@ -26,18 +24,34 @@ while ($row = mysql_fetch_assoc($item_prefix)) {
 </form>
 
 <?php 
+
+$numSkus = $_POST['numSkus'];
+$catPref = $_POST['catPref'];
+$varFlag = $_POST['varFlag'];
+
 if ($_POST['numSkus'] == 0){
 echo '<tr><td colspan="2"><font color="red">Please enter the number of skus to generate!</font></td></tr>';
 }
 elseif ($_POST['numSkus'] > 0){
 	echo '<tr><td colspan="2"><font color="green">You entered the correct number</font></td></tr>';
-	echo '<tr><td colspan="2">You entered '. $_POST['numSkus'] .' skus and selected the '. $_POST['catPref'] .' prefix</td></tr>';
-	while($i <= $_POST['numSkus']) {
-		echo $_POST['catPref'] .'-'. $topSkuNum +1;
-		$i++;
+	echo '<tr><td colspan="2">You entered '. $numSkus .' skus and selected the '. $catPref .' prefix</td></tr>';
+	$topPrefNum_check =  'Select parent_sku FROM item_alloc where sku_prefix ="'.$catPref.'" order by parent_sku desc limit 1';
+	$topPrefNum = mysql_query($topPrefNum_check, $DbLink);
+	$topPrefNum = mysql_fetch_assoc($topPrefNum);
+	$topPrefNum = explode('-',$topPrefNum['parent_sku']);
+	//echo $topPrefNum[1];
+	
+	while($i <= $numSkus) {
+	$topPrefNum[1] = $topPrefNum[1]+1;
+	$newSku = implode('-', array($catPref, $topPrefNum[1]));
+	echo '<tr><td>Generated: </td><td>'.$newSku.'</td></tr>';
+	$i++;
 	}
 }
 
 ?>
 
 </table>
+
+<?php 
+?>
