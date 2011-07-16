15f0b9e8a2f20d99ac80362caeea1e8d8c250010
r00tdenied <josh@ariel.net>
2011-07-16 11:08:22 -0700

    db conn

--- skugen.php
+++ skugen.php
@@ -6,8 +6,14 @@ dbconn(DB_ADDRESS, DB_NAME, DB_USER, DB_PASSWORD);
 $item_prefix_query='SELECT * from item_prefix';
 $item_prefix=mysql_query($item_prefix_query,$DbLink);
 
-while ($row = mysql_fetch_assoc($item_prefix)) {
-	echo $row['prefix'];
-}
-
 ?>
+<form>
+<select name='catagory'>
+<?php 
+
+while ($row = mysql_fetch_assoc($item_prefix)) {
+	echo '<option value="'. $row['sku_prefix'] .'">' . $row['description'] .'('. $row['sku_prefix'] .')</option>';
+}
+?>
+</select>
+</form>
