6e44a378972c6be2853bb1d7630238e7b5fd8914
r00tdenied <josh@ariel.net>
2011-07-19 17:03:39 -0700

    Fix search bugs, added some validation

--- controller/f_multi_item_search.php
+++ controller/f_multi_item_search.php
@@ -14,7 +14,7 @@ function multi_item_search($parentSku, $catPref, $itemType, $itemStatus) {
 		$item_data = mysql_query($item_query, $DbLink);
 	}
 	
-	elseif($catPref != '' && $itemType == '' && $itemStatus ==''){
+	if($catPref != '' && $itemType == '' && $itemStatus ==''){
 		//Query for catPref only
 		$item_query = "	SELECT 	im.parent_sku,
 							 	im.item_sku, 
@@ -27,7 +27,7 @@ function multi_item_search($parentSku, $catPref, $itemType, $itemStatus) {
 		
 	}
 	
-	elseif($catPref == '' && $itemType == '' && $itemStatus !=''){
+	if($catPref == '' && $itemType == '' && $itemStatus !=''){
 		//Query for itemStatus only
 		$item_query = "	SELECT 	im.parent_sku,
 							 	im.item_sku, 
@@ -40,7 +40,7 @@ function multi_item_search($parentSku, $catPref, $itemType, $itemStatus) {
 		
 	}
 	
-	elseif($catPref == '' && $itemType != '' && $itemStatus ==''){
+	if($catPref == '' && $itemType != '' && $itemStatus ==''){
 		//Query for itemType only
 		$item_query = "	SELECT 	im.parent_sku,
 							 	im.item_sku, 
@@ -52,6 +52,43 @@ function multi_item_search($parentSku, $catPref, $itemType, $itemStatus) {
 						where pref.prefix_type ='$itemType'";
 		$item_data = mysql_query($item_query, $DbLink);
 		
+	}
+	
+	if( $parentSku != '' && $catPref == '' && $itemType == '' && $itemStatus ==''){
+		//Query for parentSku only
+		$item_query = "	SELECT 	im.parent_sku,
+							 	im.item_sku, 
+							 	im.master_title, 
+							 	im.variant_flag
+						from item_master im
+						join item_alloc alloc on im.parent_sku = alloc.parent_sku
+						where im.parent_sku ='$parentSku'";
+		$item_data = mysql_query($item_query, $DbLink);
+		
+	}
+	
+	if(isset($parentSku) && $catPref != ''){
+		//Only allow parentSku field
+		echo '<br/><table class="table_main">';
+		echo '<tr><td style="color:red;text-align:center;">Search by Parent SKU and Item Prefix is disallowed</td></tr>';
+		echo '</table>';
+		exit;
+	}
+	
+	if(isset($parentSku) && $itemType != ''){
+		//Only allow parentSku field
+		echo '<br/><table class="table_main">';
+		echo '<tr><td style="color:red;text-align:center;">Search by Parent SKU and Item Type is disallowed</td></tr>';
+		echo '</table>';
+		exit;
+	}
+	
+		if(isset($parentSku) && $itemStatus != ''){
+		//Only allow parentSku field
+		echo '<br/><table class="table_main">';
+		echo '<tr><td style="color:red;text-align:center;">Search by Parent SKU and Item Status is disallowed</td></tr>';
+		echo '</table>';
+		exit;
 	}
 	
 	echo '<br/>';
