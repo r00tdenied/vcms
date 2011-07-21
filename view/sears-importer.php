6f328711225c86a1ecfe5ac307d6a70c0df14a01
r00tdenied <josh@ariel.net>
2011-07-21 15:17:41 -0700

    fix function use

--- view/sears-importer.php
+++ view/sears-importer.php
@@ -14,13 +14,17 @@
 </tr>
 </form>
 <?php 
-if($_POST['scan']) {
+if($_POST['scan']=='scan') {
 	OM_Sears_Import('1');
 	}
 
-elseif($_POST['fetch']) {
+elseif($_POST['fetch']=='fetch') {
 	OM_Sears_Import('2');
 }	
+
+else {
+	echo "<tr><td style='text-align: center;' colspan='2'>Please click <i>Scan for Orders</i> or <i>Fetch Scanned Orders</i></td</tr>";
+}
 
 ?>
 
