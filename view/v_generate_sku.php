f758b47c82d4358bc80f8379e55aebb1a7264488
r00tdenied <josh@ariel.net>
2011-08-02 11:25:53 -0700

    sku gen table and css

--- view/v_generate_sku.php
+++ view/v_generate_sku.php
@@ -1,12 +1,26 @@
 <form action="?p=vCMS" method="post">
-<table class="table_main" >
+<table class="table_main">
 <td><h3>Generate New Skus</h3></td>
 </table>
 <table class="table_main">
-<tr><td>Select item category:</td><td>
-<?php item_prefix_select('0') ?>
-</td></tr>
-<tr><td>Total items to create:</td><td><input type="text" name="numSkus" /></td></tr>
-<tr><td>Build items with variation:</td><td><select name='varFlag'><option value='0'>Disabled</option><option value='1'>Enabled</option></select>
-<tr><td style="text-align:center;" colspan="2"><input type="hidden" name="process" value="generateSku"></input><input type="submit" name="submit" value="Generate Skus"></form></td></tr>
+<tr>
+	<td>Select item category:</td>
+	<td><?php item_prefix_select('0') ?></td>
+	<td class="table_row_button" rowspan="3">
+		<input type="hidden" name="process" value="generateSku"/>
+		<input type="submit" name="submit" value="Generate Skus"></td>
+</tr>
+<tr>
+	<td>Total items to create:</td>
+	<td><input type="text" name="numSkus" /></td>
+</tr>
+<tr>
+	<td>Build items with variation:</td>
+	<td>
+		<select name='varFlag'>
+			<option value='0'>Disabled</option>
+			<option value='1'>Enabled</option>
+		</select>	
+</tr>
+
 </form>
