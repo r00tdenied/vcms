06afb3982078eb29c9339516ed8846a4aa310d74
r00tdenied <josh@ariel.net>
2011-08-02 11:38:50 -0700

    field formatting

--- view/v_generate_sku.php
+++ view/v_generate_sku.php
@@ -4,18 +4,18 @@
 </table>
 <table class="table_main">
 <tr>
-	<td>Select item category:</td>
+	<td><b>Select item prefix:</td>
 	<td><?php item_prefix_select('0') ?></td>
 	<td class="table_row_button" rowspan="3">
 		<input type="hidden" name="process" value="generateSku"/>
 		<input type="submit" name="submit" value="Generate Skus"></td>
 </tr>
 <tr>
-	<td>Total items to create:</td>
+	<td><b>Total items to create:</b></td>
 	<td><input type="text" name="numSkus" /></td>
 </tr>
 <tr>
-	<td>Build items with variation:</td>
+	<td><b>Build items with variation:</b></td>
 	<td>
 		<select name='varFlag'>
 			<option value='0'>Disabled</option>
