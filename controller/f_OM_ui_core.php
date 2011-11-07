e0efe2530dd04b572bad1e94e74ee4e99c5f47be
r00tdenied <josh@ariel.net>
2011-11-07 11:12:23 -0800

    using new db

--- controller/f_OM_ui_core.php
+++ controller/f_OM_ui_core.php
@@ -83,7 +83,7 @@ function OM_InnovExport_Search($trigger)
 			$sql = "use QAData select * from Orders ord 
 					join [Order Details] od on ord.OrderNumber = od.OrderNumber
 					where ord.InnovExport ='2'
-					and od.SKU not in ('Shipping','Sales Tax 1','Product','Sales Tax 3')
+					and od.SKU not in ('Shipping','Sales Tax 1','Product','Sales Tax 3','Sales Tax 2')
 					order by OrderDate desc ";
 
 			$result=odbc_exec($connect,$sql);
